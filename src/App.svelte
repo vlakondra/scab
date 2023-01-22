44bcfb751592e8fd0ee0264744d01bd76f81b2a8
vlakondra <kondrashkin.wladimir@yandex.ru>
2023-01-22 22:39:00 +0500

    did fonts

--- src/App.svelte
+++ src/App.svelte
@@ -4,56 +4,75 @@
 
   //https://github.com/js-cookie/js-cookie
   import Cookies from "js-cookie";
+  import Fa from "svelte-fa";
+  import { faArrowCircleUp } from "@fortawesome/free-solid-svg-icons";
 
   // alert(window.ttt);
   let a = 5;
   console.log(Cookies.get());
-  let b = Cookies.get("SL_G_WPT_TO");
+  let b = Cookies.get("api_token"); //   "SL_G_WPT_TO");
+  console.log("api ", b);
 </script>
 
 <main>
   <div>
     {a}
-    <p>
-      {b}
-    </p>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite!!! + Svelte!!!</h1>
 
-  <div class="card">
-    <Counter />
+    <p>{b}</p>
+    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"
+      ><img src="/vite.svg" class="logo" alt="Vite Logo" /></a
+    ><a href="https://svelte.dev" target="_blank" rel="noreferrer"
+      ><img src={svelteLogo} class="logo svelte" alt="Svelte Logo" /></a
+    >
+  </div>
+  <h1 class="capt">123 Слово ъйюяь! ! ! + Слово 2 ! ! !</h1>
+  <div class="totop-box">
+    <Fa icon={faArrowCircleUp} color="#6565ed" size="2.5x" />
   </div>
 
+  <div class="card"><Counter /></div>
   <p on:click={() => (a += 5)} on:keypress={() => null}>
     Check out <a
       href="https://github.com/sveltejs/kit#readme"
       target="_blank"
       rel="noreferrer">SvelteKit</a
-    >, the official Svelte app framework powered by Vite!
+    >, the official Svelte app framework powered by Vite !
   </p>
-
   <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
 </main>
 
 <style>
+  :global(body) {
+    font-family: Montserrat, sans-serif;
+    /* font-synthesis: none;
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-text-size-adjust: 100%; */
+  }
+  .capt {
+    text-align: center;
+    /* font-family: Montserrat, sans-serif; */
+    /* font-weight: 400; */
+  }
+
   .logo {
     height: 6em;
     padding: 1.5em;
     will-change: filter;
   }
+
   .logo:hover {
     filter: drop-shadow(0 0 2em #646cffaa);
   }
+
   .logo.svelte:hover {
     filter: drop-shadow(0 0 2em #ff3e00aa);
   }
+
   .read-the-docs {
     color: #888;
+    /* font-family: Montserrat; */
+    font-weight: 700;
   }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import './app.css'
+import "@fontsource/montserrat/cyrillic.css" //"/src/assets/index_montser.css"; //Загрузка шрифтов
+import "@fontsource/montserrat/latin.css"
 import App from './App.svelte'
 
-const app = new App({
-  target: document.getElementById('app'),
-})
+const app=new App( {
+    target: document.getElementById('app'),
+  }
+
+);
 
 export default app
