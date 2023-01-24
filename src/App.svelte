d3ce5b93f67ec89424bf75eb8c2b9c715c826ffe
vlakondra <kondrashkin.wladimir@yandex.ru>
2023-01-24 12:56:39 +0500

    did login form - bulma

--- src/App.svelte
+++ src/App.svelte
@@ -9,40 +9,26 @@
 
   //https://github.com/svelte-add/bulma
   // alert(window.ttt);
-  let a = 5;
-  console.log(Cookies.get());
-  let b = Cookies.get("SL_G_WPT_TO"); //   "SL_G_WPT_TO");
-  console.log("api ", b);
 
-  Cookies.set("mycookie", a.toString(), { expires: 7, path: "" });
-  let c = Cookies.get("mycookie");
-  console.log("c", c);
+  import Login from "./Comps/login.svelte";
+
+  console.log(Cookies.get());
+  // let b = Cookies.get("SL_G_WPT_TO"); //   "SL_G_WPT_TO");
+  // console.log("api ", b);
+
+  // Cookies.set("mycookie", "??!!", { expires: 7, path: "" });
+  // let c = Cookies.get("mycookie");
+  // console.log("c", c);
 </script>
 
 <main>
-  <div>
-    {a}
-
-    <p>{b}</p>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"
-      ><img src="/vite.svg" class="logo" alt="Vite Logo" /></a
-    ><a href="https://svelte.dev" target="_blank" rel="noreferrer"
-      ><img src={svelteLogo} class="logo svelte" alt="Svelte Logo" /></a
-    >
-  </div>
   <h1 class="title">123 Слово ъйюяь! ! ! + Слово 2 ! ! !</h1>
-  <div class="totop-box">
+  <div>
     <Fa icon={faArrowCircleUp} color="#6565ed" size="2.5x" />
   </div>
 
-  <div class="card"><Counter /></div>
-  <p on:click={() => (a += 5)} on:keypress={() => null}>
-    Check out <a
-      href="https://github.com/sveltejs/kit#readme"
-      target="_blank"
-      rel="noreferrer">SvelteKit</a
-    >, the official Svelte app framework powered by Vite !
-  </p>
+  <Login />
+
   <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
 </main>
 
@@ -63,22 +49,8 @@
     border: 3px solid blue;
   }
 
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-
   .read-the-docs {
-    color: $text-color;
+    // color: $test-color;
     font-weight: 700;
   }
 </style>
---
+++ src/Comps/login.svelte
@@ -0,0 +1,197 @@
+<script>
+    import Fa from "svelte-fa";
+    import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+    import { faLock } from "@fortawesome/free-solid-svg-icons";
+    import { faExclamationTriangle } from "@fortawesome/free-solid-svg-icons";
+
+    import {
+        useForm,
+        Hint,
+        HintGroup,
+        validators,
+        email,
+        required,
+        minLength,
+    } from "svelte-use-form";
+
+    const form = useForm();
+
+    let result;
+    let login_err;
+    let rjson;
+    $: if (login_err) {
+        console.log("reserr", login_err);
+        //сделать здесь cookie???
+        // if (rjson) console.log("res", rjson);
+    }
+    async function authenticate() {
+        // Gathered from: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+        let url = "https://api-1.ursei.su/access_token";
+        const data = new URLSearchParams();
+
+        const clientId = "studentlk";
+        const clientSecret = "studentlk";
+        const scopes = "basic studentlk";
+
+        data.append("grant_type", "password");
+        data.append("client_id", clientId);
+        data.append("client_secret", clientSecret);
+        data.append("scope", scopes);
+        data.append("username", $form.email.value);
+        data.append("password", $form.password.value);
+
+        let response = await fetch(url, {
+            method: "post",
+            mode: "cors", // no-cors, *cors, same-origin,
+            cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+            //credentials: "same-origin",
+            headers: {
+                Accept: "1.0",
+                "Content-Type": "application/x-www-form-urlencoded",
+                //'Content-Type': 'application/json'
+            },
+            body: data,
+        });
+        console.log("resp", response.status);
+        // One additional option is to use:
+        // ... = await response.json();
+        // But since we're printing out the response in an HTML element,
+        // it is convenient to await the `.text()` promise.
+        //??rjson = await response.json(); //text();
+
+        // This next line is verbose, but it's meant to demonstrate
+        // what happens when we want to use a reactive value change
+        // to bind our new information using `$: if(data) {...}`
+        //let resp = text;
+        return response.json();
+    }
+
+    function submitHandler() {
+        //https://sveltesociety.dev/recipes/design-patterns/authentication-with-svelte
+        /* This promise needs to be awaited somewhere --
+         * either in the HTML body via `{#await}` tags,
+         * in a `<script>` tag, or in an imported `.js` module.
+         */
+        try {
+            result = authenticate();
+        } catch (error) {
+            console.log("EE", error.message);
+        }
+
+        // authenticate()
+        //     .then((data) => {
+        //         result = data;
+        //         console.log("data", data);
+        //     })
+        //     .catch((error) => {
+        //         login_err = error.message;
+        //         console.log("err", error);
+        //     });
+    }
+</script>
+
+<div>
+    <form on:submit|preventDefault={submitHandler} class="login-form" use:form>
+        <div class="field">
+            <label for="email" class="label">Email:</label>
+            <div class="control has-icons-left has-icons-right">
+                <input
+                    class="input is-danger"
+                    type="email"
+                    name="email"
+                    use:validators={[required, email]}
+                    placeholder="Email"
+                />
+                <span class="icon is-small  is-left">
+                    <Fa icon={faEnvelope} />
+                </span>
+                <span class="icon is-small is-right">
+                    <Fa icon={faExclamationTriangle} />
+                </span>
+            </div>
+            <div class="hint-container">
+                <HintGroup for="email">
+                    <Hint class="help is-danger" on="required">
+                        Это обязательное поле
+                    </Hint>
+                    <Hint class="help is-danger" on="email" hideWhenRequired>
+                        Введен некорректный адрес
+                    </Hint>
+                </HintGroup>
+            </div>
+        </div>
+
+        <div class="field">
+            <label for="password" class="label">Пароль:</label>
+            <div class="control has-icons-left has-icons-right">
+                <input
+                    class="input is-danger"
+                    type="password"
+                    name="password"
+                    use:validators={[required]}
+                    placeholder="Пароль"
+                />
+                <span class="icon is-small  is-left">
+                    <Fa icon={faLock} />
+                </span>
+                <span class="icon is-small is-right">
+                    <Fa icon={faExclamationTriangle} />
+                </span>
+            </div>
+            <div class="hint-container">
+                <Hint class="help is-danger" for="password" on="required">
+                    Это обязательное поле
+                </Hint>
+            </div>
+        </div>
+
+        <button class="button is-info" disabled={!$form.valid}>Вход</button>
+    </form>
+</div>
+<div>
+    {#if result === undefined}
+        <p>???</p>
+    {:else}
+        {#await result}
+            <div><span>Logging in...</span></div>
+        {:then value}
+            <div><span>{value["access_token"]}</span></div>
+        {:catch error}
+            <div>!!!<span>{error}</span></div>
+        {/await}
+    {/if}
+</div>
+
+<style lang="scss">
+    // :global(.svelte-use-form-hint) {
+    //     color: blue;
+    // }
+
+    :global(.help.is-danger) {
+        color: blue;
+    }
+    label.label {
+        text-align: left;
+        font-weight: 400;
+        margin-bottom: 0;
+    }
+    .login-form {
+        display: flex;
+        flex-direction: column;
+
+        padding: 20px;
+        border: 1px solid yellow;
+        // tmp
+        /* background-color: $background; */
+        width: 500px;
+        margin: 0 auto;
+    }
+    .hint-container {
+        height: 20px;
+    }
+    .login-form button {
+        align-self: flex-end;
+        width: 25%;
+        // margin-top: 1rem;
+    }
+</style>
